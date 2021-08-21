bcada562f7b1eb067e5419209c4b47111f7143e5
Mckenzie Irwin <irwinmck@gmail.com>
2021-08-20 23:11:55 -0400

    learning how to use gatsby static images

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,10 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "gatsby tutorial",
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp"
+  ]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 
 // Components
 import Layout from '../components/layout';
@@ -7,6 +8,10 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../assets/surprised.jpg"
+      />
     </Layout>
   );
 };
