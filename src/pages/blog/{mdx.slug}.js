9fb9b83b9873cbe05eba794602abf0b6a1da7afb
Mckenzie Irwin <irwinmck@gmail.com>
2021-08-21 17:28:56 -0400

    blog post refinement + addition of dynamic GatsbyImage(s) based on the specific post.

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 // Components
 import Layout from '../../components/layout';
@@ -8,10 +9,21 @@ import Layout from '../../components/layout';
 const BlogPost = ({ data }) => {
   const { title, date } = data.mdx.frontmatter;
   const { body } = data.mdx;
+  const image = getImage(data.mdx.frontmatter.hero_image);
 
   return (
     <Layout pageTitle={title}>
       <p>{date}</p>
+      <GatsbyImage
+        image={image}
+        alt={data.mdx.frontmatter.hero_image_alt}
+      />
+      <p>
+        Photo Credit:{" "}
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
       <MDXRenderer>{body}</MDXRenderer>
     </Layout>
   );
@@ -23,7 +35,15 @@ export const query = graphql`
       frontmatter {
         title
         date(formatString: "MMMM D, YYYY")
-      }
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+      },
       body
     }
   }
